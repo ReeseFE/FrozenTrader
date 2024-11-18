aa27b0497dcced10c8eb5076d5a5302eb9f9a0a8
lishaowei <544383961@qq.com>
2024-11-18 23:38:02 +0800

    好玩

--- src/app/page.js
+++ src/app/page.js
@@ -3,46 +3,58 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <main className={styles.main}>
+          <Image
+              className={styles.logo}
+              src='/snowBg.png'
+              alt='Next.js Logo'
+              width={500}
+              height={500}
+              priority
+          />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+          <div className={styles.grid}>
+              <a
+                  href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+                  className={styles.card}
+                  target='_blank'
+                  rel='noopener noreferrer'
+              >
+                  <h2>
+                      Demo <span>-&gt;</span>
+                  </h2>
+                  <p>
+                      Find in-depth information about Next.js features and API.
+                  </p>
+              </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+              <a
+                  href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+                  className={styles.card}
+                  target='_blank'
+                  rel='noopener noreferrer'
+              >
+                  <h2>
+                      Docs <span>-&gt;</span>
+                  </h2>
+                  <p>Explore starter templates for Next.js.</p>
+              </a>
+
+              <a
+                  href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+                  className={styles.card}
+                  target='_blank'
+                  rel='noopener noreferrer'
+              >
+                  <h2>
+                      Dev <span>-&gt;</span>
+                  </h2>
+                  <p>
+                      Instantly deploy your Next.js site to a shareable URL with
+                      Vercel.
+                  </p>
+              </a>
+          </div>
+      </main>
   );
 }
