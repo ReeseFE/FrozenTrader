0564fe16373d956243907dc758f0cb66ff0441a5
lishaowei <544383961@qq.com>
2024-11-21 23:14:40 +0800

    动态引入Monaco避免报错

--- src/app/playground/page.jsx
+++ src/app/playground/page.jsx
@@ -1,33 +1,14 @@
 'use client';
-import React, { useRef, useEffect } from 'react';
-import * as monaco from 'monaco-editor';
+import React from 'react';
+import dynamic from 'next/dynamic';
 
-const MonacoEditor = ({ value, language, onChange }) => {
-  const editorRef = useRef(null);
-  const containerRef = useRef(null);
+// 动态引入MonacoEditor组件，并禁用SSR
+const MonacoEditor = dynamic(() => import('@/components/MonacoEditor'), {
+    ssr: false,
+});
 
-  useEffect(() => {
-    if (containerRef.current) {
-      editorRef.current = monaco.editor.create(containerRef.current, {
-        value,
-        language: 'python',
-        theme: 'vs-dark',
-      });
-
-      editorRef.current.onDidChangeModelContent(() => {
-        const newValue = editorRef.current.getValue();
-        // onChange(newValue);
-      });
-    }
-
-    return () => {
-      if (editorRef.current) {
-        editorRef.current.dispose();
-      }
-    };
-  }, [containerRef]);
-
-  return <div ref={containerRef} style={{ height: '100vh', width: '50%' }} />;
+const Playground = () => {
+    return <MonacoEditor />;
 };
 
-export default MonacoEditor;
+export default Playground;
