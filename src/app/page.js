c0632bc6585b912e0b59a5b77f9b93874bdcd6b7
Mxyzptlk-Z <ericcccc_z@outlook.com>
2024-12-18 00:08:11 +0800

    继续窄屏适配

--- src/app/page.js
+++ src/app/page.js
@@ -39,22 +39,6 @@ export default function Home() {
             <CountUpCard endValue={500} title="Daily Trades" />
             <CountUpCard endValue={50} title="Trading Strategies" />
           </div>
-          {/* <Link href='/playground' className={styles.topButton}>
-            <button className={styles.cssbuttons}>
-              <span>
-                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M0 0h24v24H0z" fill="none"></path>
-                  <path d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z" fill="currentColor"></path>
-                </svg>
-                Get Started
-              </span>
-            </button>
-          </Link> */}
-        </main>
-      </section>
-
-      <section className={styles.section} id="section-1">
-        <div className={styles.secondPage}>
           <Link href='/playground' className={styles.topButton}>
             <button className={styles.cssbuttons}>
               <span>
@@ -66,7 +50,15 @@ export default function Home() {
               </span>
             </button>
           </Link>
-        </div>
+        </main>
+      </section>
+
+      <section className={styles.section} id="section-1">
+      <main className={styles.main}>
+          <p className={styles.title}>
+            A privacy-focused, easy-to-use quantitative research platform
+          </p>
+        </main>
       </section>
 
       <section className={styles.section} id="section-2">
