02442bd886e1e8878de4cc125e7068519295c547
Mxyzptlk-Z <ericcccc_z@outlook.com>
2025-01-05 23:11:24 +0800

    更新ModuleCard文字描述

--- src/constant/module.js
+++ src/constant/module.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { AiOutlineCode } from "react-icons/ai";
-import { BsFillBookmarkCheckFill, BsPen } from "react-icons/bs";
-import { BiRocket } from "react-icons/bi";
+import { BsFillBookmarkCheckFill } from "react-icons/bs";
+import { BsCurrencyDollar } from "react-icons/bs";
 import { FaLaptopCode } from "react-icons/fa";
+import { GoDatabase } from "react-icons/go";
+import { TbHierarchy } from "react-icons/tb";
+import { PiChartPieSlice } from "react-icons/pi";
+import { RiStockLine } from "react-icons/ri";
 
  export const modules = [
   {
     id: 0,
-    title: "Web App development",
-    description: `We'll build you a website that's so good, it'll make all the other websites jealous. Trust me, not kidding.`,
-    icon: <AiOutlineCode className="h-8 w-8 text-gray-500" />,
+    title: "Data Architecture",
+    description: `The system implements a flexible and robust data storage and processing (ETL) pipeline, designed to handle various usage scenarios.`,
+    icon: <GoDatabase className="h-8 w-8 text-gray-500" />,
     pattern: {
       y: 16,
       squares: [
@@ -20,9 +23,9 @@ import { FaLaptopCode } from "react-icons/fa";
   },
   {
     id: 1,
-    title: "Web Design",
-    description: `Your website will be so beautiful, you'll want to frame it and hang it on your wall (but please don't).`,
-    icon: <BsPen className="h-8 w-8 text-gray-500" />,
+    title: "Event-Driven Backtesting",
+    description: `Frozen provides a event-driven backtesting engine based on historical data, optimized for fast runtime.`,
+    icon: <RiStockLine className="h-8 w-8 text-gray-500" />,
     pattern: {
       y: -6,
       squares: [
@@ -33,9 +36,9 @@ import { FaLaptopCode } from "react-icons/fa";
   },
   {
     id: 2,
-    title: "Deployments and Hosting",
-    description: `Get your website out there in the world, where it belongs, with our lightning-fast deployment services.`,
-    icon: <BiRocket className="h-8 w-8 text-gray-500 stroke-gray-500" />,
+    title: "Factor Calculation",
+    description: `A powerful expression engine that allows you to write concise factor formulae instead of complex python codes.`,
+    icon: <TbHierarchy className="h-8 w-8 text-gray-500 stroke-gray-500" />,
     pattern: {
       y: 32,
       squares: [
@@ -46,24 +49,9 @@ import { FaLaptopCode } from "react-icons/fa";
   },
   {
     id: 3,
-    title: "Maintenence",
-    description: `We keep your website running like a well-oiled machine. From optimizing performance to ensuring security, we've got you covered`,
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        stroke="currentColor"
-        className="w-8 h-8 text-gray-500 stroke-gray-500"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z"
-        />
-      </svg>
-    ),
+    title: "Portfolio Optimization",
+    description: `Make decisions with the risk model to achieve optimal asset allocation weights based on your desired constraints.`,
+    icon: <PiChartPieSlice className="h-8 w-8 text-gray-500" />,
     pattern: {
       y: 22,
       squares: [[0, 1]],
@@ -71,8 +59,8 @@ import { FaLaptopCode } from "react-icons/fa";
   },
   {
     id: 4,
-    title: "Full-Stack Development",
-    description: `Take your website to the next level with our full-stack development services. We've got all the skills to make your vision a reality.`,
+    title: "Parameter Tuning",
+    description: `Leverage our optimization framework for parameter tuning tasks by searching the parameter space for parameters that give the best performance.`,
     icon: <FaLaptopCode className="h-8 w-8 text-gray-500" />,
     pattern: {
       y: 16,
@@ -84,9 +72,9 @@ import { FaLaptopCode } from "react-icons/fa";
   },
   {
     id: 5,
-    title: "And everything else",
-    description: `Ecommerce, Landing pages, Back-end heavy, Dashboards. You name it, we've done it AND we'll do it for you.`,
-    icon: <BsFillBookmarkCheckFill className="h-8 w-8 text-green-500" />,
+    title: "Live Trading",
+    description: `Consider additional complexities including market impact, liquidity constraint, execution costs, and real-time data processing.`,
+    icon: <BsCurrencyDollar className="h-8 w-8 text-gray-500" />,
     pattern: {
       y: -6,
       squares: [
